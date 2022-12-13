42cc89860f9dc68608359977b7c9ee0d6cb2293e
Ysl Ron <yslron1000@gmail.com>
2022-12-14 04:51:31 +0800

    Gene Write Whole Website and AI Architecture

---
+++ controllers/openaiController.js
@@ -0,0 +1,54 @@
+const { Configuration, OpenAIApi } = require('openai');
+
+// Create a new Configuration object with the API key
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+// Create a new OpenAIApi object using the Configuration object
+const openai = new OpenAIApi(configuration);
+
+// Async function to generate an image
+const generateImage = async (req, res) => {
+  // Destructure the prompt and size from the request body
+  
+  const { prompt} = req.body;
+
+  // Set the image size based on the size parameter
+  
+
+  try {
+    // Use the OpenAI API to generate an image with the given prompt and size
+    const response = await openai.createImage({
+      prompt,
+      n: 1,
+      size: '512x512',
+    });
+
+    // Get the URL of the generated image
+    const imageUrl = response.data.data[0].url;
+
+    // Return a success response with the image URL
+    res.status(200).json({
+      success: true,
+      data: imageUrl,
+    });
+  } catch (error) {
+    // If there was an error, log it
+    if (error.response) {
+      console.log(error.response.status);
+      console.log(error.response.data);
+    } else {
+      console.log(error.message);
+    }
+
+    // Return an error response
+    res.status(400).json({
+      success: false,
+      error: 'The image could not be generated',
+    });
+  }
+};
+
+// Export the generateImage function
+module.exports = { generateImage };
